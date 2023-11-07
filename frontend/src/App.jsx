d2ed7b0a145fb64e1bf9070f31c256e4bb1fe36f
Nikita <kofman.niki@gmail.com>
2023-11-07 19:32:22 +0100

    creation of login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <p className="text-4xl">SERVICE DESK</p>
+      <p className="text-4xl mt-10 mb-10">AUTHENTIFICATE YOURSELF</p>
+      <div className="flex flex-col items-center justify-center">
+        USERNAME
+        <input type="text" className="mt-5 mb-5" />
+        PASSWORD
+        <input type="text" className="mt-5 mb-5" />
+        <button
+          type="submit"
+          className="bg-sky-100 mt-10 w-20 text-black pr-4 flex items-center justify-center"
+        >
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
